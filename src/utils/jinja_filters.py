f389b8d502e4410fc21b5f2b38f22ab803242f3e
Félix Piédallu <felix@piedallu.me>
2025-12-28 13:59:09 +0100

    utils: jinja_filters: Add stable_shuffle jinja filter for monthly-stable shuffling
    This allows e.g resolver list to be shuffled but stable across a month.
    
    This month rhythm might change someday.

--- src/utils/jinja_filters.py
+++ src/utils/jinja_filters.py
@@ -18,7 +18,12 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
+import hashlib
+import datetime
 import json
+import random
+from pathlib import Path
+from typing import TypeVar
 
 import toml
 import yaml
@@ -71,3 +76,25 @@ def to_toml(value) -> str:
     Serialize to string an object to Toml
     """
     return toml.dumps(value)
+
+
+T = TypeVar("T")
+
+
+def stable_shuffle(values: list[T]) -> list[T]:
+    """
+    Use a seed derived from the machine id and current month
+    This way, the shuffle is random but should be stable accross the same month
+    and make sure the regenconf is idempotent (at least during the same month)
+    i.e. that it doesn't re-shuffle the file everytime we run the regenconf
+    """
+
+    seed_txt = ""
+    if (machine_id := Path("/etc/machine-id")).exists():
+        seed_txt += machine_id.read_text()
+    seed_txt += datetime.datetime.now().strftime("%m%Y")
+
+    seed = hashlib.md5(seed_txt.encode("utf-8")).digest()
+    random.seed(seed)
+    random.shuffle(values)
+    return values
